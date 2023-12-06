94c29effec976fb18a4e1d131c1ad33f0a491c78
Nguyen Hiep Loi <loi.nguyen@HBG.COM>
2023-12-06 18:34:55 +0700

    brand to speech

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        outfit:["Outfit",'san-serif'],
+        playfair:["Playfair Display",'serif'],
+        PlusJakarta:["Plus Jakarta Sans",'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
