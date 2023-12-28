a457cb276e9c35f874c8dbfa6529de8293bc2d51
anhocva214 <anhocva214@gmail.com>
2023-12-28 10:51:18 +0700

    update

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 docker image rm text_to_speech_user:latest
 docker build -t text_to_speech_user:latest .
 docker rm -f text_to_speech_user
-docker run -it -d --name text_to_speech_user -p 5173:5173 --restart always --network apps --link nginx-proxy --link letsencrypt-nginx-proxy --env TZ=Asia/Ho_Chi_Minh -e VIRTUAL_HOST="text-to-speech.daithienhien.vn" -e VIRTUAL_PORT=5173 -e LETSENCRYPT_HOST="text-to-speech.daithienhien.vn" -e LETSENCRYPT_EMAIL="daithienhien.vn@gmail.com" text_to_speech_user:latest
+docker run -it -d --name text_to_speech_user -p 3006:5173 --restart always --network apps --link nginx-proxy --link letsencrypt-nginx-proxy --env TZ=Asia/Ho_Chi_Minh -e VIRTUAL_HOST="text-to-speech.daithienhien.vn" -e VIRTUAL_PORT=5173 -e LETSENCRYPT_HOST="text-to-speech.daithienhien.vn" -e LETSENCRYPT_EMAIL="daithienhien.vn@gmail.com" text_to_speech_user:latest
